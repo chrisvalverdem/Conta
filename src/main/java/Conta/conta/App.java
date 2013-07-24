cdceaed85ce9f1c48bddc88020f7882ea760a0af
cguillen <cguilen@testingsoft.com>
2013-07-24 16:32:41 -0500

    Estructura del Proyecto

---
+++ src/main/java/Conta/conta/App.java
@@ -0,0 +1,13 @@
+package Conta.conta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
