8cb49ae954a6047e72408d53f614be9377a737da
jahlopez <jlopez@cecropiasolutions.com>
2013-07-29 11:41:23 -0600

    Cambios debido a los mensajes y a la escritura del log

--- src/main/java/com/ts/main/InterpreteMandatos.java
+++ src/main/java/com/ts/main/InterpreteMandatos.java
@@ -12,6 +12,7 @@ import com.ts.objects.Repo;
 public class InterpreteMandatos {
 	 	static String comando="";
 	 	static String[] parametros;
+	 	public static boolean estadoFuncion= false;	
 	 	
 	public static void ObtenerParametros(String cadena){
 		int contador=0; 				
@@ -21,7 +22,7 @@ public class InterpreteMandatos {
 		comando= tokens.nextToken();	
 		while(tokens.hasMoreTokens()){
 			parametros[contador]= tokens.nextToken().toString();
-			System.out.println(contador + " " + parametros[contador]);
+			System.out.println("Token " + contador + " valor: " + parametros[contador]);
 			contador++; 
 			}//while
 		}//asigna	 	
@@ -39,58 +40,133 @@ public class InterpreteMandatos {
 		 try {
 	        	dato = br.readLine();
 	            cadena= dato.toLowerCase().toString();
-	   		 	System.out.println("La cadena introducida fue: "+ cadena + "\n");
+	   		 	System.out.println("La cadena introducida fue: "+ cadena);
 	   		 	ObtenerParametros(cadena);
-	            
+	   		 	System.out.print("Comando Real:" + comando + "\n");	
+	   		 
 	   		 switch(comando){	   		 
 	   		 case "exit": 
 	   			 ArchivoLog.crearLog(comando);
 	   			 System.exit(0);
 	   		 break; 	   		 
-	   		 case "crear_colaborador": 
-	   			System.out.print("Comando real:" + comando + "\n");	   			
+	   		 case "crear_colaborador":   			
 	   			Repo.AgregarColaborador(parametros[0], Integer.parseInt(parametros[1].toString()));	   			
-	   			ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   		
+	   		//Se guarda en archivo solo en su ejecucion exitosa
+	   			if(estadoFuncion){
+		   		 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			}else{
+	   			 System.out.print("El Colaborador no se agrego " + estadoFuncion + "\n");	
+	   			}   		
 	   		 break;	   		 
-	   		 case "crear_edificio": 
-	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");	   			 
+	   		 case "crear_edificio": 	   			 
 	   			 Repo.AgregarEdificio(parametros[0]);	   			
-	   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString());	   		 	   		 
+	   		
+	   		//Se guarda en archivo solo en su ejecucion exitosa
+	   			if(estadoFuncion){
+		   		 ArchivoLog.crearLog(comando + " " + parametros[0].toString());	   		 	   		 
+		   		}else{
+		   		 System.out.print("El Edificio no se agrego" );	
+		   		} 
 	   		 break;	   		 
 	   		 case "crear_proyecto": 
-	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
 	   			 Repo.AgregarProyecto(parametros[0]);	   			
-	   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString());	 
+	   			
+	   		//Se guarda en archivo solo en su ejecucion exitosa
+	   			if(estadoFuncion){
+	   			ArchivoLog.crearLog(comando + " " + parametros[0].toString());	   		 	   		 
+			   	}else{
+			   	System.out.print("El Proyecto no se agrego");	
+			   	}   
 	   		 break;	   		 
 	   		 case "crear_compañia":
-	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
 	   			Repo.AgregarCompania(parametros[0], Integer.parseInt(parametros[1].toString()));	   			
-	   			ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());
+	   			
+	   			//Se guarda en archivo solo en su ejecucion exitosa
+	   			if(estadoFuncion){
+	   			ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());		   		 	   		 
+				}else{
+				System.out.print("El Proyecto no se agrego");	
+				}
 	   		 break;	   		 
 	   		 case "crear_activo":   
-	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
-	   			Repo.AgregarActivo(parametros[0], Integer.parseInt(parametros[1].toString()));	   			
+	   			 Repo.AgregarActivo(parametros[0], Integer.parseInt(parametros[1].toString()));	   			
+	   		
+	   		//Se guarda en archivo solo en su ejecucion exitosa
+	   			if(estadoFuncion){
 	   			ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());
+	   			}else{
+				System.out.print("El Activo no se agrego");	
+				}
 	   		 break;
 	   		 
-	   		 case "aumentar_salario": System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   		 case "aumentar_salario": 
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   			
+	   		 
+	   		 //Se guarda en archivo solo en su ejecucion exitosa
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }
 	   		 break;
 	   		 
 	   		 case "calcular_salario":
-	   			   			 
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   			
+	   		 //Se guarda en archivo solo en su ejecucion exitosa	 
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }	   		
 	   		 break;
 	   		 
-	   		 case "calcular_retenciones_fuente":	System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   		 case "calcular_retenciones_fuente":	
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   		
+	   		 
+	   		 //Se guarda en archivo solo en su ejecucion exitosa
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }	   		 
 	   		 break;
 	   		 
-	   		 case "mostrar_retenciones_fuente":	System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
-	   		 break;
+	   		 case "mostrar_retenciones_fuente":	
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   			
+	   		 //Se guarda en archivo solo en su ejecucion exitosa
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }	   		 
+	   		break;
 	   		 
-	   		 case "desvincular_colaborador_proyecto":	System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
-	   		 break;
+	   		 case "desvincular_colaborador_proyecto":	
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   		
+	   		 //Se guarda en archivo solo en su ejecucion exitosa
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }	   		
+	   		break;
 	   		 
-	   		 case "vincular_colaborador_proyecto":	System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
-	   		 break;
+	   		 case "vincular_colaborador_proyecto":	
+	   			 System.out.println("El comando introducido no se encuentra implementado aun: "+ comando +"\n");
+	   		 
+	   		 //Se guarda en archivo solo en su ejecucion exitosa
+	   			 if(estadoFuncion){
+		   			 ArchivoLog.crearLog(comando + " " + parametros[0].toString() + " " + parametros[1].toString());	   			
+	   			 }else{
+	   				System.out.print("No se realizo el proceso");	
+	   			 }
+	   		break;
 	   		
 	   		 
 	   		 default:System.err.print("\n" + "Comando desconocido. Favor introducirlo nuevamente:" + "\n"); break;
