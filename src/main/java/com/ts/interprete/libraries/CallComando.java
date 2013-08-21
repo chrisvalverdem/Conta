1421fee89da06426b5e7de7df96ca8977f8755b7
cguillen <cguilen@testingsoft.com>
2013-08-21 15:08:05 -0500

    Se arregla repo para que los keys devuelvan datos

---
+++ src/main/java/com/ts/interprete/libraries/CallComando.java
@@ -0,0 +1,15 @@
+package com.ts.interprete.libraries;
+
+public class CallComando extends Comando{
+		
+	public CallComando(Expression expression) {
+		super(expression);
+	}
+
+	@Override
+	public void execute()
+	{
+		
+	}
+
+}
